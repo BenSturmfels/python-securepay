56fb03d273c8bd8a5ac87f84598172025d90f646
Ben Sturmfels <ben@sturm.com.au>
2022-02-09 23:59:01 +1100

    Flit trial.

--- src/securepay/__init__.py
+++ src/securepay/__init__.py
@@ -1,3 +1,7 @@
+"""Interface to the Securepay credit card gateway."""
+
+__version__ = '1.0.0'
+
 from .securepay import (
     LIVE_API_URL, TEST_API_URL, pay_by_cc, refund, SecurePayError, GatewayError,
     PaymentError)
